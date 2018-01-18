7377585ed1b15d01a8d7287c8a3de88af9394497
Marc Gallet <zertrin@gmail.com>
2018-01-18 14:19:20 +0800

    Add PriorityQueue class in zkpytb.utils

--- zkpytb/utils.py
+++ zkpytb/utils.py
@@ -6,6 +6,8 @@ Date: 2017-06
 """
 
 import hashlib
+import heapq
+import itertools
 import logging
 import subprocess
 
@@ -47,3 +49,35 @@ def get_git_hash(rev='HEAD'):
         git_hash = git_out.strip()
 
     return git_hash
+
+
+class PriorityQueue:
+    """From https://docs.python.org/3/library/heapq.html#priority-queue-implementation-notes"""
+    def __init__(self):
+        self.pq = []                         # list of entries arranged in a heap
+        self.entry_finder = {}               # mapping of tasks to entries
+        self.REMOVED = '<removed-task>'      # placeholder for a removed task
+        self.counter = itertools.count()     # unique sequence count
+
+    def add_task(self, task, priority=0):
+        'Add a new task or update the priority of an existing task'
+        if task in self.entry_finder:
+            self.remove_task(task)
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        heapq.heappush(self.pq, entry)
+
+    def remove_task(self, task):
+        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = self.entry_finder.pop(task)
+        entry[-1] = self.REMOVED
+
+    def pop_task(self):
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, count, task = heapq.heappop(self.pq)
+            if task is not self.REMOVED:
+                del self.entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
